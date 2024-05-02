4a0d0f8bdccfb9b48b78794a861dcd6ad259c33e
Sumedh2709 <120184582+Sumedh2709@users.noreply.github.com>
2024-05-02 19:28:26 +0530

    NS_exp2.cpp

--- NS_exp2.cpp
+++ NS_exp2.cpp
@@ -1,84 +1,128 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-string encrypt(const string& text, const string& key) 
+//Encription function
+string Encryption(int no_rows, int len_key, int len_msg, string msg, int col_val[])
 {
-    int num_cols = key.length();
-
-    string padded_text = text + string((num_cols - text.length() % num_cols) % num_cols, ' ');
-
-    vector<string> matrix;
-
-    for (int i = 0; i < padded_text.length(); i += num_cols) 
-	{
-        matrix.push_back(padded_text.substr(i, num_cols));
-    }
-    
-	vector< pair<char,string> > sorted_columns;
-    
-	for (int i = 0; i < num_cols; ++i) 
-	{
-        sorted_columns.push_back(make_pair(key[i], matrix[i]));
-    }
-    
-	sort(sorted_columns.begin(), sorted_columns.end());
-    
-	string encrypted_text = "";
-    for (const auto& col : sorted_columns) 
-	{
-        encrypted_text += col.second;
-    }
-    return encrypted_text;
-}
-
-string decrypt(const string& text, const string& key) 
-{
-    int num_cols = key.length();
-
-    int num_rows = text.length() / num_cols;
-
-    vector<string> matrix(num_cols);
-  
-    for (int i = 0; i < num_cols; ++i)
-	{
-        matrix[i] = text.substr(i * num_rows, num_rows);
-    }
-    
-	vector<pair<char, string>> sorted_columns;
-    
-	for (int i = 0; i < num_cols; ++i) 
-	{
-        sorted_columns.emplace_back(key[i], matrix[i]);
-    }
-    
-	sort(sorted_columns.begin(), sorted_columns.end());
-    
-	string decrypted_text;
-    
-	for (int i = 0; i < num_rows; ++i) 
-	{
-        for (const auto& col : sorted_columns)
-		{
-            decrypted_text += col.second[i];
+    int x = 0;
+    char enc_mat[no_rows + 1][len_key];
+  //creating the matrix 
+    for (int i = 0; i < no_rows + 1; i++)
+    {
+        for (int j = 0; j < len_key; j++)
+        {
+          //initializes the positions with '_' after the end of message
+            if (x >= len_msg)
+            {
+                enc_mat[i][j] = '_';
+            }
+            else
+            {
+                enc_mat[i][j] = msg[x];
+            }
+            x++;
         }
     }
-    
-	return decrypted_text;
-
+  
+    int t = 1;
+    string cipher = "";
+  //finding the cipher text according to the value of col_val matrix
+    while (t <= len_key)
+    {
+        for (int i = 0; i < len_key; i++)
+        {
+            int k = col_val[i];
+            if (k == t)
+            {
+                for (int j = 0; j < no_rows + 1; j++)
+                {
+                    cipher += enc_mat[j][i];
+                }
+                t++;
+            }
+        }
+    }
+    return cipher;
 }
 
-int main() {
-    string plain_text, key;
-    cout << "Enter plain text (all uppercase): ";
-    getline(cin, plain_text);
-    cout << "Enter the key (unique numbers from 1 to n to define sequence): ";
-    getline(cin, key);
+//decryption function
+string Decryption(int no_rows, int len_key, string cipher,int col_val[])
+{
+    char dec_mat[no_rows + 1][len_key];
+    int x = 0,t = 1;
+  //rearrange the matrix according to the col_val
+    while (t <= len_key)
+    {
+        for (int i = 0; i < len_key; i++)
+        {
+            int k = col_val[i];
+            if (k == t)
+            {
+                for (int j = 0; j < no_rows + 1; j++)
+                {
+                    dec_mat[j][i]=cipher[x];
+                    x++;
+                }
+                t++;
+            }
+        }
+    }
 
-    string encrypted = encrypt(plain_text, key);
-    cout << "Encrypted text: " << encrypted << endl;
+    string message = "";
+    for (int i = 0; i < no_rows + 1; i++)
+    {
+        for (int j = 0; j < len_key; j++)
+        {
+          //replacing the '_' with space
+            if (dec_mat[i][j] == '_')
+            {
+                dec_mat[i][j] = ' ';
+            }
+            message += dec_mat[i][j];
+        }
+    }
+    return message;
+}
 
-    string decrypted = decrypt(encrypted, key);
-    cout << "Decrypted text: " << decrypted << endl;
+int main()
+{
+  //message
+    string msg = "pict_123456";
+  //key
+    string key = "HACK";
 
-    return 0;
+    int len_key = key.length();
+    int len_msg = msg.length();
+
+    int val = 1,count = 0,ind;
+
+    int col_val[len_key];
+  //intializing col_val matrix with 0
+    memset(col_val, 0, sizeof(col_val));
+  //numbering the key alphabets according to its ACII value
+    while (count < len_key)
+    {
+        int min = 999;
+        for (int i = 0; i < len_key; i++)
+        {
+            if ((min > int(key[i])) && (col_val[i] == 0))
+            {
+                min = int(key[i]);
+                ind = i;
+            }
+        }
+        col_val[ind] = val;
+        count++;
+        val++;
+    }
+
+    int no_rows = len_msg / len_key;
+//encrypted text
+    string cipher_text = " ";
+    cipher_text = Encryption(no_rows, len_key, len_msg, msg, col_val);
+    cout << "Encrypted Message : " << cipher_text << endl;
+//decrypted text
+    string original_msg = " ";
+    original_msg = Decryption(no_rows, len_key, cipher_text,col_val);
+    cout << "Decrypted Message : " << original_msg << endl;
 }
