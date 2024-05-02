38e032c5b78b8365e5880af8a8b6444207bdcadd
Sumedh2709 <sumedhkasat32@gmail.com>
2024-05-02 17:34:38 +0530

    init commit

---
+++ NS_exp3.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+string encrypt(const string& text, int shift) 
+{
+    string encrypted_text = "";
+    for (int i =0; text[i] != '\0';i++) 
+	{
+        encrypted_text += (text[i] - 'A' + shift) % 26 + 'A';
+    }
+    return encrypted_text;
+}
+
+string decrypt(const string& text, int shift) 
+{
+    string decrypted_text = "";
+    for (int i =0; text[i] != '\0';i++) 
+	{
+        decrypted_text += (text[i] - 'A' - shift + 26) % 26 + 'A';
+    }
+    return decrypted_text;
+}
+
+int main() {
+    string plain_text;
+    int shift_value;
+
+    cout << "Enter plain text (all uppercase): ";
+    cin >> plain_text;
+
+    cout << "Enter shift value (Enter 3 for Caesar cipher): ";
+    cin >> shift_value;
+
+    string encrypted = encrypt(plain_text, shift_value);
+    cout << "Encrypted text: " << encrypted << endl;
+
+    string decrypted = decrypt(encrypted, shift_value);
+    cout << "Decrypted text: " << decrypted << endl;
+
+    return 0;
+}
